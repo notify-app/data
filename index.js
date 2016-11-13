b1b6f706e9079472633fc05e8a5238644f2afbc4
Luca Tabone <taboneluca3@gmail.com>
2016-11-13 12:55:23 +0100

    Include messages

--- index.js
+++ index.js
@@ -15,16 +15,30 @@ for (let i = 0; i < keys.length; i++) {
 
 Promise.all(promises)
   .then(() => {
+    console.log('states created')
     return creator.create(notifyStore.types.STATES,
       require('./src/data/states'))
   })
   .then(() => {
+    console.log('users created')
     return creator.create(notifyStore.types.USERS,
       require('./src/data/users'))
   })
   .then(() => {
+    console.log('rooms created')
     return creator.create(notifyStore.types.ROOMS,
       require('./src/data/rooms'))
+  })
+  .then(() => {
+    console.log('messages created')
+    return creator.create(notifyStore.types.MESSAGES,
+      require('./src/data/messages'))
+  })
+  .then(() => {
+    console.log('ready.')
+  })
+  .catch((err) => {
+    console.error('error:', err)
   })
 
 
--- src/data/messages.js
+++ src/data/messages.js
@@ -1,13 +1,38 @@
 'use strict'
 
 module.exports = [{
-  content: 'Hi',
+  content: 'message 1',
   user: 'a0',
   room: 'a0',
   unread: []
-},{
-  content: 'Hello :D',
+}, {
+  content: 'message 2',
+  user: 'a0',
+  room: 'a0',
+  unread: []
+}, {
+  content: 'message 2.1',
   user: 'a1',
   room: 'a0',
+  unread: []
+}, {
+  content: 'message 3',
+  user: 'a0',
+  room: 'a1',
+  unread: []
+}, {
+  content: 'message 4',
+  user: 'a0',
+  room: 'a1',
+  unread: []
+}, {
+  content: 'message 5',
+  user: 'a1',
+  room: 'a5',
+  unread: []
+}, {
+  content: 'message 6',
+  user: 'a1',
+  room: 'a5',
   unread: []
 }]
