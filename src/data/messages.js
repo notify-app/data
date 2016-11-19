dd8ed2c3f3e2b23ceceb9393ce4c43e1a1391abe
Luca Tabone <taboneluca3@gmail.com>
2016-11-19 12:26:47 +0100

    Describe messages

--- src/data/messages.js
+++ src/data/messages.js
@@ -1,36 +1,43 @@
 'use strict'
 
 module.exports = [{
+  id: 'a0', // mine room 1
   content: 'message 1',
   user: 'a0',
   room: 'a0',
   unread: []
 }, {
+  id: 'a1', // mine room 1
   content: 'message 2',
   user: 'a0',
   room: 'a0',
   unread: []
 }, {
+  id: 'a2', // m8 room 1
   content: 'message 2.1',
   user: 'a1',
   room: 'a0',
   unread: []
 }, {
+  id: 'a3', // mine room 2
   content: 'message 3',
   user: 'a0',
   room: 'a1',
   unread: []
 }, {
+  id: 'a4', // mine room 2
   content: 'message 4',
   user: 'a0',
   room: 'a1',
   unread: []
 }, {
+  id: 'a5', // m8 room 3
   content: 'message 5',
   user: 'a1',
   room: 'a5',
   unread: []
 }, {
+  id: 'a6', // m8 room 3
   content: 'message 6',
   user: 'a1',
   room: 'a5',
